c9f380c5ce03140e452b6f4c617b76baffaebdc2
ehm <emrahhalacoglu@gmail.com>
2022-11-03 13:57:46 +0300

    While loop solution added.

--- Code.java
+++ Code.java
@@ -5,23 +5,55 @@ public class Code {
 
         System.out.print("Basamak Sayisini Giriniz : ");
         int n=input.nextInt();
-        //ÜÇGEN YAPIMI İÇİN GEREKLİ KOD BLOĞU
+        //FOR DÖNGÜSÜ İLE ÜÇGEN YAPIMI İÇİN GEREKLİ KOD BLOĞU
         for(int i=1;i<=n;i++){
-            for(int j=i;j<=n;j++){
+            for(int j=i;j<n;j++){
                 System.out.print(" ");
             }
             for(int k=n+2*i;k>=n+2;k--)
             System.out.print("*");
             System.out.println();
         }
-        /*//DIAMOND İÇİN AÇILMASI GEREKEN KOD BLOĞU
+        /*//WHİLE DÖNGÜSÜ İLE ÜÇGEN YAPIMI İÇİN GEREKLİ KOD BLOĞU
+        int i=1;
+        while(i<=n){
+            int j=i;
+            while(j<n){
+                System.out.print(" ");
+                j++;
+            }
+            int k=n+2*i;
+            while(k>=n+2){
+                System.out.print("*");
+                k--;
+            }
+            System.out.println();
+            i++;
+        }*/
+        //FOR DÖNGÜSÜ İLE DIAMOND YAPIMI İÇİN AÇILMASI GEREKEN KOD BLOĞU
         for(int i=2;i<=n;i++){
-            for(int j=n-i+1;j<=n;j++){
+            for(int j=n-i+1;j<n;j++){
                 System.out.print(" ");
             }
             for(int k=i;k<=2*n-i;k++)
             System.out.print("*");
             System.out.println();
+        }
+        /*//WHİLE DÖNGÜSÜ İLE DIAMOND YAPIMI İÇİN AÇILMASI GEREKEN KOD BLOĞU
+        int i=1;
+        while(i<=n){
+            int j=n-i+1;
+            while(j<n){
+                System.out.print(" ");
+                j++;
+            }
+            int k=i;
+            while(k<=2*n-i){
+                System.out.print("*");
+                k++;
+            }
+            System.out.println();
+            i++;
         }*/
     }
     
